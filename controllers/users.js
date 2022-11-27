4c26053b3a7ddc442da8a690c9998bd3c226046d
Leonid Marz <l3m4rz@gmail.com>
2022-11-27 10:19:42 +0300

    feat: Создать контроллеры и роуты для пользователей

---
+++ controllers/users.js
@@ -0,0 +1,20 @@
+const Users = require('../models/user');
+
+module.exports.getUsers = (req, res) => {
+  Users.find({})
+    .then((users) => res.send({ data: users }))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+};
+
+module.exports.getUserId = (req, res) => {
+  Users.findById(req.params.userId)
+    .then((user) => res.send({ data: user }))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+};
+
+module.exports.createUser = (req, res) => {
+  const { name, about, avatar } = req.body;
+  Users.create({ name, about, avatar })
+    .then((user) => res.send({ data: user }))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+};
---
+++ routes/users.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const { getUsers, createUser, getUserId } = require('../controllers/users');
+
+router.get('/', getUsers);
+
+router.get('/:userId', getUserId);
+
+router.post('/', createUser);
+
+module.exports = router;
